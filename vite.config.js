6d24780cd4319aef112edecd12702cf70ef52e4d
GuiMayor <villawolflobo@gmail.com>
2026-02-07 22:58:48 +0100

    Configura Tailwind correctamente para producción

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // <-- rutas absolutas para que CSS y JS se carguen en Vetel
+  base: './', // rutas relativas
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react()
   ],
 })
-
-
