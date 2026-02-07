31d23f75e4e04ef5a3be70e0521f24eae4c8f9a9
GuiMayor <villawolflobo@gmail.com>
2026-02-07 23:20:36 +0100

    fix: tailwind v4 vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // rutas relativas
   plugins: [
-    react()
+    react(),
+    tailwindcss(),
   ],
 })
+
