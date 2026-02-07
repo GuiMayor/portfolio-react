5e041c76f17eb31edd000504188e4843c74cc801
GuiMayor <villawolflobo@gmail.com>
2026-02-07 20:28:00 +0100

    Corrige rutas relativas en vite.config.js para Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // <-- esto hace que Vercel use rutas relativas y se vea tu portafolio
+  base: './', // <-- rutas absolutas para que CSS y JS se carguen en Vetel
   plugins: [
     react({
       babel: {
