2c4e3d11c5d6695b0bc388aaa6460a55a8728329
GuiMayor <villawolflobo@gmail.com>
2026-02-12 20:03:58 +0100

    Actualizar build para GitHub Pages con base correcta

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portfolio-react/',  // <-- agrega esta línea
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
-
